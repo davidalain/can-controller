ded6ac57630123741dd2e00a0e253d3cf953ac96
David Alain <davidalain89@gmail.com>
2017-06-09 00:43:23 -0300

    Correção da saída do estado EOF.

--- Projeto exemplo/Projeto/can_decoder.v
+++ Projeto exemplo/Projeto/can_decoder.v
@@ -467,7 +467,7 @@ always @(posedge clock or posedge reset)
 begin
 if(reset)
 	state_eof <= 1'b0;
-else if(go_state_eof | go_state_error_flags) //Sai do estado se a flag do próximo estiver ativa!
+else if(go_state_overload_flags | go_state_interframe | go_state_error_flags) //Sai do estado se a flag do próximo estiver ativa!
 	state_eof <= 1'b0;
 else if(go_state_eof)
 	state_eof <= 1'b1; //Entra no estado!
